266671c63c609117ae9ded583de17df0ce404b23
Janus Jordan <jljordan2@mmm.com>
2021-11-21 02:08:29 +0800

    Initial Commit

---
+++ TwitterApplication/TwitterApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApplicationApp.swift
+//  TwitterApplication
+//
+//  Created by Janus Jordan on 11/21/21.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
